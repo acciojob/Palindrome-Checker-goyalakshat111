677ec15bdb71b2251d7049a80d8fbb2c2bf5a4bb
Akshat Goyal <goyalakshat111@gmail.com>
2025-03-19 22:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-	for(let t of str){
-		if(t==t.split("").reverse().join("")){
+		if(str==str.split("").reverse().join("")){
 			return true;
 		}
 		else{
 			return false;
 		}
-	}
-
 }
 module.exports = palindrome
